882280be6bfbca733614914516d713f388e04741
Niranjana1009 <niranjanac10@gmail.com>
2025-11-14 19:35:44 +0530

    modify approach

--- 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.java
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.java
@@ -1,3 +1,4 @@
+//approach 1 - simulate flow
 class Solution {
     public String winningPlayer(int x, int y) {
         boolean flag=true;
@@ -7,5 +8,13 @@ class Solution {
             y-=4;
         }
         return flag?"Bob":"Alice";
+    }
+} 
+
+//approach 2 - based on parity
+class Solution {
+    public String winningPlayer(int x, int y) {
+        int turn = Math.min(x, y/4);
+        return (turn % 2 == 1) ? "Alice": "Bob";
     }
 }
