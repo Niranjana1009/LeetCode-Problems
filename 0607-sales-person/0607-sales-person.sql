0d5322d4d5322ef47073a4f683734070e1fffd50
Niranjana Devi P C <niranjanac10@gmail.com>
2026-04-17 11:27:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,3 +1,3 @@
 SELECT name FROM SalesPerson WHERE sales_id NOT IN (SELECT sales_id FROM Orders WHERE com_id=(
-    SELECT com_id FROM Company WHERE name="RED"
+    SELECT com_id FROM Company WHERE name like "%RED%" AND name LIKE "%RED" AND name LIKE "RED%"
 ));
